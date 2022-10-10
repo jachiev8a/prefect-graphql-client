85f4d5160bb102b851fc8b32f2416cd8038047ee
Javier Ochoa <jachiev.ochoa@gmail.com>
2022-10-10 17:47:59 -0500

    improve: add README and add argparser to manage arguments

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # prefect-graphql-client
+
+## Introduction
+This prefect client has some utils to interact with the Prefect Cloud service.
+These utils include reporting to check all scheduled workflows in place,
+showing if they are active or not and what cron configuration they have.
+
+## Installation
+
+### Environment
+```bash
+# copy .env.example to .env
+cp .env.example .env
+
+# This example file has 2 variables as placeholders.
+# You need to edit these variables with actual values from cloud.
+PREFECT_API_KEY=xXxXx
+PREFECT_TENANT_ID=xXxXx
+
+# NOTE: get the api key and the tenant ID from the cloud environment
+```
+
+### Python
+```bash
+# create python virtual env
+python -m virtualenv .venv
+
+# activate virtualenv (linux)
+. .venv/bin/activate
+
+# (venv) install dependencies
+python -m pip install -r requirements.txt
+
+# configure your prefect backend to "cloud"
+prefect backend cloud
+```
+
+## How to use the client
+
+```bash
+# activate virtualenv (linux)
+. .venv/bin/activate
+
+# (venv) run main script with help command 
+# to see what you are able to do with it.
+python query_executor.py -h
+
+# Examples:
+python query_executor.py --print-schedule-active
+python query_executor.py --print-schedule-config
+```
--- query_executor.py
+++ query_executor.py
@@ -1,16 +1,76 @@
+import argparse
+
 from decouple import config
 
 from models.PrefectCloudApiModel import PrefectCloudApiModel
 
-PREFECT_API_KEY = config("PREFECT_API_KEY")
-PREFECT_TENANT_ID = config("PREFECT_TENANT_ID")
 
-client = PrefectCloudApiModel(
-    api_key=PREFECT_API_KEY,
-    tenant_id=PREFECT_TENANT_ID
-)
+# ---------------
+# MAIN
+# ---------------
+def main():
+    """Main Function"""
 
-client.print_report_schedule_active()
-print("")
-print("")
-client.print_report_schedule_configurations(sort_by="schedule")
+    help_description = (
+        "pending doc..."
+    )
+
+    # Script Argument Parser
+    parser = argparse.ArgumentParser(description=help_description)
+    # parser.add_argument(
+    #     "-e",
+    #     "--email",
+    #     default=None,
+    #     required=False,
+    #     metavar="EMAIL",
+    #     help="email to be used to authenticate to 1password account",
+    # )
+    parser.add_argument(
+        "--print-schedule-active",
+        action="store_true",
+        required=False,
+        help=(
+            "flag to print a report for all workflows with current schedule active."
+        ),
+    )
+    parser.add_argument(
+        "--print-schedule-config",
+        action="store_true",
+        required=False,
+        help=(
+            "flag to print a report for all schedule configurations. "
+            "If a workflows has been configured with schedule, it will be shown here."
+        ),
+    )
+
+    args = parser.parse_args()
+    print_schedule_active = args.print_schedule_active
+    print_schedule_config = args.print_schedule_config
+
+    any_print_selected = (
+        print_schedule_active or
+        print_schedule_config
+    )
+
+    if not any_print_selected:
+        exit("ERROR: no option to print was selected!")
+
+    prefect_api_key = config("PREFECT_API_KEY")
+    prefect_tenant_id = config("PREFECT_TENANT_ID")
+
+    client = PrefectCloudApiModel(
+        api_key=prefect_api_key,
+        tenant_id=prefect_tenant_id
+    )
+
+    if print_schedule_active:
+        client.print_report_schedule_active()
+        print("")
+        print("")
+
+    if print_schedule_config:
+        client.print_report_schedule_configurations(sort_by="schedule")
+
+
+if __name__ == "__main__":
+    main()
